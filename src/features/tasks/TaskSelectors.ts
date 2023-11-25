2c0ed0c333bd5fc0e7f9cdbf6c626d431ccab727
nurainiamelia <nurainiamelia2@gmail.com>
2023-11-25 13:21:39 +0700

    Refactor raw data

--- src/features/tasks/TaskSelectors.ts
+++ src/features/tasks/TaskSelectors.ts
@@ -1,9 +1,11 @@
 import { selector } from 'recoil'
 import { tasksState } from './TaskAtoms'
 import type { Task } from '../../types'
+import {SelectorKeys} from '../../constants/recoilKeys'
 
 export const notStartedTasksSelector = selector<Task[]>({
-  key: 'not_started_tasks',
+  // key: 'not_started_tasks',
+  key: SelectorKeys.NOT_STARTED_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 1
@@ -12,7 +14,8 @@ export const notStartedTasksSelector = selector<Task[]>({
 })
 
 export const inProgressTasksSelector = selector<Task[]>({
-  key: 'in_progress_tasks',
+  // key: 'in_progress_tasks',
+  key: SelectorKeys.IN_PROGRESS_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 2
@@ -21,7 +24,8 @@ export const inProgressTasksSelector = selector<Task[]>({
 })
 
 export const waitingTasksSelector = selector<Task[]>({
-  key: 'waiting_tasks',
+  // key: 'waiting_tasks',
+  key: SelectorKeys.WAITING_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 3
@@ -30,7 +34,8 @@ export const waitingTasksSelector = selector<Task[]>({
 })
 
 export const uncompletedTasksSelector = selector<Task[]>({
-  key: 'uncompleted_tasks',
+  // key: 'uncompleted_tasks', 
+  key: SelectorKeys.UNCOMPLETED_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder !== 4
@@ -39,7 +44,8 @@ export const uncompletedTasksSelector = selector<Task[]>({
 })
 
 export const completedTasksSelector = selector<Task[]>({
-  key: 'completed_tasks',
+  // key: 'completed_tasks',
+  key: SelectorKeys.COMPLETED_TASKS,
   get: ({ get }) => {
     return get(tasksState).filter((task) => {
       return task.progressOrder === 4
